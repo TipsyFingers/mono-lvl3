8b6f84569c314561c97c12b31eafeaa31f73c4b7
TipsyFingers <kruno_a@hotmail.com>
2017-05-30 15:26:36 +0200

    Maps

---
+++ mono-lvl3.Models/ModelsMappingProfile.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using mono_lvl3.DAL.EntityModels;
+using mono_lvl3.Models.Common;
+using mono_lvl3.Models.DomainModels;
+
+namespace mono_lvl3.Models
+{
+    public class ModelsMappingProfile : Profile
+    {
+        public ModelsMappingProfile()
+        {
+            CreateMap<Album, AlbumPOCO>().ReverseMap();
+            CreateMap<Album, IAlbum>().ReverseMap();
+            CreateMap<IAlbum, AlbumPOCO>().ReverseMap();
+
+            CreateMap<Artist, ArtistPOCO>().ReverseMap();
+            CreateMap<Artist, IArtist>().ReverseMap();
+            CreateMap<IArtist, ArtistPOCO>().ReverseMap();
+
+            CreateMap<Song, SongPOCO>().ReverseMap();
+            CreateMap<Song, ISong>().ReverseMap();
+            CreateMap<ISong, SongPOCO>().ReverseMap();
+        }
+    }
+}
